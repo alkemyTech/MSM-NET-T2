2e82b93264a5946da061a4916823c06e01b8b906
Diaz Juan <juan35427@gmail.com>
2023-11-07 11:51:40 -0300

    add swagger

--- VirtualWallet/Program.cs
+++ VirtualWallet/Program.cs
@@ -3,10 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "VirtualWallet");
+    });
+
 
 app.UseAuthorization();
 
