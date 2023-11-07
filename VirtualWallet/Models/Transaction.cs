b4c1090bf12e4f294491d0944947b11965d442cf
Diaz Juan <juan35427@gmail.com>
2023-11-07 12:06:19 -0300

    add transaction model

---
+++ VirtualWallet/Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿namespace VirtualWallet.Models
+{
+    public class Transaction
+    {
+        public int transaction_id { get; set; }
+        public decimal amount { get; set; }
+        public string concept { get; set; }
+        public DateTime date { get; set; }
+        public string type { get; set; } //Topuop, payment
+        public int account_id { get; set; } //FK a Account
+        public int user_id { get; set; }  //FK a Users
+        public int? to_account_id { get; set; } //FK a Account
+
+        //Propiedades
+        public Account account { get; set; }
+        public User user { get; set; }
+        public Account ToAccount { get; set; }
+    }
+}
