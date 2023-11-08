f20983da88c89035dac893429c9271702e5b602e
Diaz Juan <juan35427@gmail.com>
2023-11-08 11:22:11 -0300

    update transaction

--- VirtualWallet/Models/Transaction.cs
+++ VirtualWallet/Models/Transaction.cs
@@ -1,19 +1,35 @@
-﻿namespace VirtualWallet.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VirtualWallet.Models
 {
     public class Transaction
     {
-        public int transaction_id { get; set; }
-        public decimal amount { get; set; }
-        public string concept { get; set; }
-        public DateTime date { get; set; }
-        public string type { get; set; } //Topuop, payment
-        public int account_id { get; set; } //FK a Account
-        public int user_id { get; set; }  //FK a Users
-        public int? to_account_id { get; set; } //FK a Account
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TransactionId { get; set; }
 
-        //Propiedades
-        public Account account { get; set; }
-        public User user { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Amount { get; set; }
+
+        [MaxLength(255)]
+        public string Concept { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [MaxLength(255)]
+        public string Type { get; set; } //Topuop, payment
+
+        [ForeignKey("Account")]
+        public int AccountId { get; set; } //FK a Account
+        public Account Account { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }  //FK a Users
+        public User User { get; set; }
+
+        [ForeignKey("ToAccount")]
+        public int? ToAccountId { get; set; } //FK a Account
         public Account ToAccount { get; set; }
     }
 }
