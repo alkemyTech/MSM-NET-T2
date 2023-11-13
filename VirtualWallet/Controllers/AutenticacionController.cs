62b70ff527fdf1e55e33bb1eba8bf149db433483
Diaz Juan <juan35427@gmail.com>
2023-11-13 17:14:22 -0300

    add JWT

---
+++ VirtualWallet/Controllers/AutenticacionController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using VirtualWallet.DataAccess;
+using VirtualWallet.Models;
+
+namespace VirtualWallet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutenticacionController : ControllerBase
+    {
+        private readonly string secretKey;
+        private readonly VirtualWalletDbContext _context;
+        public AutenticacionController(IConfiguration config, VirtualWalletDbContext context)
+        {
+            secretKey = config.GetSection("settings:secretkey").Value;
+
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("validar")]
+        public async Task<IActionResult> Validar([FromBody] AuthModel request)
+        {
+            var users = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (users == null)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, new { token = "inexistente o inhabilitado" });
+            }
+            if (request.Password == users.Password && request.Email == users.Email)
+            {
+                var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+                var claims = new ClaimsIdentity();
+
+                claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, request.Password.ToString()));
+                claims.AddClaim(new Claim(ClaimTypes.Role, users.Role_Id == 1 ? "Admin" : "Regular"));
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = claims,
+                    Expires = DateTime.UtcNow.AddMinutes(15),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+
+                string tokenCreado = tokenHandler.WriteToken(tokenConfig);
+
+                return StatusCode(StatusCodes.Status200OK, new { token = tokenCreado });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, new { token = "inexistente o inhabilitado" });
+            }
+        }
+    }
+}
--- VirtualWallet/Program.cs
+++ VirtualWallet/Program.cs
@@ -3,15 +3,67 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using VirtualWallet;
 using VirtualWallet.DataAccess;
 using VirtualWallet.Repository;
 using VirtualWallet.Repository.Interfaces;
 using VirtualWallet.Services;
-using VirtualWallet.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json");
+var secretKey = builder.Configuration.GetSection("settings:secretkey").Value;
+var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = false,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("RegularPolicy", policy => policy.RequireRole("Regular"));
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "VirtualWallet", Version = "v1" });
+
+    // Configura la autenticación para Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            new string[] { }
+        }
+    });
+});
+
 builder.Services.AddControllers();
 
 // Swagger
@@ -31,11 +83,12 @@ builder.Services.AddScoped<ITransactionsRepository, TransactionRepository>();
 builder.Services.AddScoped<ICatalogueRepository, CatalogueRepository>();
 builder.Services.AddScoped<IFixedTermRepository, FixedTermRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddScoped<TransactionService>();
 builder.Services.AddScoped<CatalogueService>();
 builder.Services.AddScoped<FixedTermService>();
 builder.Services.AddScoped<IUserService, UserService>();
-
+//builder.Services.AddScoped<UnitOfWork>();
 
 var app = builder.Build();
 
@@ -45,6 +98,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
